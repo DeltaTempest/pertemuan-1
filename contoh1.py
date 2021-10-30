3b6aed8acf29439e9a0776377467e08326dac06c
Delta Gamers <raihan5.herwin@gmail.com>
2021-10-30 14:45:40 +0700

    test

---
+++ contoh1.py
@@ -0,0 +1,17 @@
+print("hello, world")
+name = "Herwin"
+dan = "dan"
+logika = True
+
+print(name + " " + dan)
+print(type(logika))
+
+x = 1
+y = 2
+
+x = 2.8
+print(int(x))
+
+
+print("x + y * z =", x + y * z)
+
